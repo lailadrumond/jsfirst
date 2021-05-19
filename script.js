454aa196966416ed6ec82328a020e9606ff020f0
Laila Drumond <81584683+lailadrumond@users.noreply.github.com>
2021-05-18 22:44:47 -0300

    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let nome = "Laila"
+let idade = 33
+let ano = 2021 - idade
+
+
+console.log("Meu ano de nascimento é ${ano}")
+let nome = `Laila`
+let idade = 34
+let ano = 2021 - idade
+
+
+console.log (`Meu ano de nascimento é ${ano}`)
+
+let souhumano = true
+let b = 4
+let c = 6
+
+console.log(b + c)
+
+
+
+
+
